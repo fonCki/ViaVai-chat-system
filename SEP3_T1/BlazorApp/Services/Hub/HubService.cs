070cdfa37321a479452bdf75e85e357532491df1
fonCki <alfonsoridao@gmail.com>
2022-05-10 14:11:49 +0200

    Hub Created

--- SEP3_T1/BlazorApp/Services/Hub/HubService.cs
+++ SEP3_T1/BlazorApp/Services/Hub/HubService.cs
@@ -1,5 +1,28 @@
+using System.Text.Json;
+using Entities.Model;
+using Microsoft.AspNetCore.SignalR.Client;
+
 namespace BlazorApp.Services.Hub; 
 
 public class HubService {
     
+    private const string Path = "https://localhost:7777/chat";
+
+    private HubConnection? _hubConnection;
+
+    public async Task<HubConnection> GetHubConnection() {
+        _hubConnection ??=  new HubConnectionBuilder().WithUrl(Path).Build();
+        _hubConnection.On<String>("Brodcast", ReceiveMessage);
+        return _hubConnection;
+    }
+    
+    private void ReceiveMessage(string messageInJson) {
+
+        //Convert Message from Json
+        Message message = JsonSerializer.Deserialize<Message>(messageInJson, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        
+    }
 }
